cf04c983ab8618dd53b4b2e8c3a0a210ed57456f
Jakub Guzik <guzikq@gmail.com>
2018-08-22 20:22:40 +0200

    1scena
    Początek 1 sceny

---
+++ Magisterka Gra/Assets/Skrypty/1Poczatek/Main_poczatek.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_poczatek : MonoBehaviour {
+    public GameObject napis;
+    public GameObject przycisk1;
+    public GameObject przycisk2;
+	// Use this for initialization
+	void Start () {
+        napis.SetActive(true);
+        przycisk1.SetActive(false);
+        przycisk2.SetActive(false);
+        StartCoroutine(Przycisk());
+    }
+
+    IEnumerator Przycisk()
+    {
+        yield return new WaitForSeconds(5);
+        napis.SetActive(false);
+        przycisk1.SetActive(true);
+        przycisk2.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
