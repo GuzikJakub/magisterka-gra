3cb6e02f9f85ed0ef04d2ae0a881b16190410d94
Jakub Guzik <jakub.guzik@firma.interia.pl>
2019-08-29 18:09:42 +0200

    Kolejne full gotowe sceny

---
+++ Magisterka Gra/Assets/Skrypty/1Poczatek/Main_poczatek_wyjdz.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_poczatek_wyjdz : MonoBehaviour {
+    public GameObject napis;
+    public GameObject przycisk1;
+    public GameObject przycisk2;
+//    public GameObject Sebastian;
+	// Use this for initialization
+	void Start () {
+ //       Sebastian.SetActive(true);
+        napis.GetComponent<UnityEngine.UI.Text>().text = "Wybierz poziom";
+        napis.SetActive(true);
+        przycisk1.SetActive(true);
+        przycisk2.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Magisterka Gra/Assets/Skrypty/TextAnim.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAnim : MonoBehaviour
+{
+    public float showTime = 0.01f;
+    public string textshow;
+    public GameObject mytext;
+    public GameObject scena;
+    public GameObject scena1;
+    public GameObject scenaown;
+
+    void Start()
+    {
+  //      mytext = GetComponent<UnityEngine.UI.Text>().text;
+        textshow = mytext.GetComponent<UnityEngine.UI.Text>().text;
+        Debug.Log(textshow);
+        mytext.GetComponent<UnityEngine.UI.Text>().text = "";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in textshow.ToCharArray())
+        {
+            mytext.GetComponent<UnityEngine.UI.Text>().text += letter;
+//            Debug.Log(letter);
+            yield return 0;
+            yield return new WaitForSeconds(showTime);
+        }
+        yield return new WaitForSeconds(2);
+        scena.SetActive(true);
+        scena1.SetActive(true);
+        scenaown.SetActive(false);
+    }
+}
